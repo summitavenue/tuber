2bf351a1075fb852700573d2351009a18e33a5f9
Charles Lai <cjl223@cornell.edu>
2015-09-19 01:48:52 -0400

    rename uber_code

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+from uber import * 
+
+app = Flask(__name__)
+ 
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 	
+    resp = twilio.twiml.Response()
+    uber_response = me()
+    resp.message("Hello! Here is your uber profile: " + uber_response.text)
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
